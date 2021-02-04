f430e7e96cdaf85d19d5d7fb46f9779a8e8e689c
Raunak Anand <rsanand@ucdavis.edu>
2021-02-04 18:08:21 +0530

    Added files

---
+++ app.js
@@ -0,0 +1,48 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+//
+const greetings = ['Im good you little piece of shit'];
+
+const weather = ['Weather is fine', 'You need a tan'];
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function () {
+	console.log('voice is activated, you can use the microphone');
+
+};
+
+
+recognition.onresult = function(event){
+	const current = event.resultIndex;
+
+	const transcript = event.results[current][0].transcript;
+	content.textContent = transcript;
+	readOutLoud(transcript)
+};
+
+// add the listener to the btn
+
+btn.addEventListener('click', () =>{
+	recognition.start();
+});
+
+function readOutLoud(message){
+	const speech = new SpeechSynthesisUtterance();
+
+	speech.text = 'i dont know what you said, idiot';
+
+	if(message.includes('how are you')){
+		const finalText = greetings[Math.floor(Math.random() * greetings.length)];
+		speech.text = finalText; 
+	}
+
+	speech.volume = 1;
+	speech.rate = 1;
+	speech.pitch = 1;
+
+
+	window.speechSynthesis.speak(speech)
+}
